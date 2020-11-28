a0048b0f8ebd15e2d55fdcc3ff27d1fdc7a7c5f1
rachael-weir <rweir@clarku.edu>
2020-11-28 18:34:31 -0500

    rachael 11/24/2020

---
+++ app/src/main/java/com/example/co_stock/IndexImage.kt
@@ -0,0 +1,19 @@
+package com.example.co_stock
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "indexTable")
+class IndexImage {
+    @PrimaryKey
+    var symbol = "symbol"
+
+    var date = ""
+    var open = 0.0f
+    var high = 0.0f
+    var low = 0.0f
+    var close = 0.0f
+    var change = 0.0f
+    var changePercent = 0.0f
+    var changeOverTime = 0.0f
+}
